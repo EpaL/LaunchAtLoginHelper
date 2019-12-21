10d007e3ffe976ac5ceb13f9c37b20e28171c523
Edward Lawford <elawford@gmail.com>
2019-12-20 23:46:32 -0500

    Added LLStrings.g

---
+++ LLStrings.h
@@ -0,0 +1,5 @@
+// strings used by LLManager and LaunchAtLoginHelper
+//
+
+#define LLURLScheme @"peakhour4.launchatlogin"
+#define LLHelperBundleIdentifier @"com.digitician.peakhour4.launchAtLoginHelper"
